1ec2dcea3398525a38e233b15c23e51f3962287f
Superchauzette <kevin.tillot@gmail.com>
2014-04-12 20:01:05 +0200

    mise a niveau apres dev et test

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-var qtouch = require("qtouch");
-var piblaster = require("pi-blaster.js");
-var converter = require("color-convert");
+var qtouch = require("./qtouch");
+var Led = require("./commandeLED");
 
+var curseurColor=0;
+var curseurLightness=0;
+var curseurON=0;
+var ON = true;
 
-// qtouch.read(function(data){
-// 	if(data=="0x00"){
-// 		 	rgb(0, 0, 0);
-// 	}
-// 	else if(data=="0x01"){
-// 	 	rgb(50, 50, 50);
-// 	}
-// 	else{
-// 		console.log(data);
-// 	}
-// });
-
-qtouch.onPressLong(function(data){
-	console.log(data);
+// sur l'appui sur bouton 0 KEY0
+qtouch.onPressUp(0, function(){
+    if(ON){ // sur le premier appui
+        var boucle = setInterval(function() {  // on boucle pour augmenter la luminosité
+            if(curseurON>100){ // Pour arrêter la boucle, le curseur va de 0 à 100, supérieur sinon il ne s'arreter pas à 100
+				clearInterval(boucle);
+				ON=false;
+			}
+            Led.setHSV(0, 0, curseurON);  
+            curseurON=curseurON+5;
+          },5);
+    }else{  // sur le second appui
+         var boucle = setInterval(function() {
+            if(curseurON<0){ // Pour arrêter la boucle, le curseur va de 0 à 100, supérieur sinon il ne s'arreter pas à 100
+				clearInterval(boucle);
+				ON=true;
+			}
+            Led.setHSV(0, 0, curseurON);
+            curseurON=curseurON-5;
+        },5);
+    }
 });
 
 
-function balayageColor(curseur){
-	hsl(1, 1, curseur/100);
-}
-
-function rgb(r, g, b){
-	piblaster.setPwm(22, r/255);
-	piblaster.setPwm(23, g/255);
-	piblaster.setPwm(24, b/255);
-}
-
-function hsl(h, s, l){
-	var rgb = converter.hsl2rgb(h,s,l); 
-	rgb(rgb[0], rgb[1], rgb[2]);
-}
-
+//lecture en continu des valeurs sur bus I2C
+qtouch.read(function(dataI2C){
+    // sur le maintien du bouton 1 balaye les couleurs
+    if(qtouch.BoutonAllumer(dataI2C,1)){
+        if(curseurColor>360) curseurColor=0; //remettre le curseur à 0
+        Led.setHSV(curseurColor, 100, 100); // Le curseur va de 0 à 360
+		curseurColor++;
+    }
+    
+     // sur le maintien du bouton 2 balaye la luninosité
+    if(qtouch.BoutonAllumer(dataI2C,2)){
+        if(curseurLightness>100) curseurLightness=0;   //remettre le curseur à 0
+        Led.setHSV(0, 0, curseurLightness); // Le curseur va de 0 à 100
+		curseurLightness++;
+        
+    }
+});
